03cb03a3189068b438635072718d0bfdb0fe6e9b
Cody Fisher <fishercody23@gmail.com>
2024-06-25 20:19:11 -0600

    All project panels are filled in with actual information, all icon links work and link you to the proper webpage/github. Mobile view still not implemented.

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
-let link1 = document.getElementById("link1");
+let site1 = document.getElementById("signupwebsite");
+let github1 = document.getElementById("signupgithub")
+let site2 = document.getElementById("landingwebsite")
+let github2 = document.getElementById("landinggithub")
+let site3 = document.getElementById("rockwebsite")
+let github3 = document.getElementById("rockgithub")
+let site4 = document.getElementById("calcwebsite")
+let github4 = document.getElementById("calcgithub")
+let site5 = document.getElementById("recipewebsite")
+let github5 = document.getElementById("recipegithub")
+let site6 = document.getElementById("etchwebsite")
+let github6 = document.getElementById("etchgithub")
 
-link1.addEventListener('click', function() {
+site1.addEventListener('click', function() {
    window.open('https://cfisher21.github.io/Sign-up-Form/', '_blank') 
 })
+
+github1.addEventListener('click', function() {
+   window.open('https://github.com/CFisher21/Sign-up-Form', '_blank')
+} )
+
+site2.addEventListener('click', function() {
+   window.open('https://cfisher21.github.io/landing-page/', '_blank') 
+})
+
+github2.addEventListener('click', function() {
+   window.open('https://github.com/CFisher21/landing-page', '_blank')
+} )
+
+site3.addEventListener('click', function() {
+   window.open('https://cfisher21.github.io/RockPaperScissors/', '_blank') 
+})
+
+github3.addEventListener('click', function() {
+   window.open('https://github.com/CFisher21/RockPaperScissors', '_blank')
+} )
+
+site4.addEventListener('click', function() {
+   window.open('https://cfisher21.github.io/Calculator/', '_blank') 
+})
+
+github4.addEventListener('click', function() {
+   window.open('https://github.com/CFisher21/Calculator', '_blank')
+} )
+
+site5.addEventListener('click', function() {
+   window.open('https://cfisher21.github.io/odin-recipes/', '_blank') 
+})
+
+github5.addEventListener('click', function() {
+   window.open('https://github.com/CFisher21/odin-recipes', '_blank')
+} )
+
+site6.addEventListener('click', function() {
+   window.open('https://cfisher21.github.io/Etch-a-Sketch/', '_blank') 
+})
+
+github6.addEventListener('click', function() {
+   window.open('https://github.com/CFisher21/Etch-a-Sketch/', '_blank')
+} )
